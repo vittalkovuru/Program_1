01d39a4fbf4a3971183cb55de8cf79299dd74912
E000542 <E000542@HW1579.cignitiglobal.com>
2017-08-08 11:19:51 +0530

    Program_1

---
+++ src/Program_1.java
@@ -0,0 +1,8 @@
+
+public class Program_1 {
+	   public static void main(String[] args) {
+		      // Prints "Hello World" in the terminal window.
+		      System.out.println("Welcome Jenkins");
+		     }
+
+}
